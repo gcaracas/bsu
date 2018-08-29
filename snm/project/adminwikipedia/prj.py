f46a8eaac4d9f336ff0c2b4e394c6454d41d4182
Gerardo Caracas <Gerardo.Caracas@kount.com>
2018-08-29 15:51:57 -0600

    data mingle

--- snm/project/adminwikipedia/prj.py
+++ snm/project/adminwikipedia/prj.py
@@ -25,14 +25,36 @@ for file in files:
         lines = f.read()
 sections = re.split("gerardocaracas\n",lines)
 
+def extract(string):
+    keys=[]
+    vals=[]
+    res=dict()
+    data = string.split('\n')
+    for eachrow in data:
+        if eachrow != '':
+            print(eachrow)
+            key, pairs =  eachrow.split(':::')
+            keys.append(key)
+            vals.append(pairs)
+    res=dict(zip(keys,vals))
+    return res
+
+
 final = []
-for list in sections:
-    # List = per block
-    ai = list.split('\n')
-    z = dict()
-    # ai = line per bloxk "a:b"
-    for list2 in ai:
-        ab = list2.split(":")
-        print(ab(1))
-        #z[ab[0]] = ab[1]
-        
+counter = 0
+for liststr in sections:
+    # Remove empty strings
+    
+    finaldict = extract(liststr)
+    counter = counter + 1
+    if counter == 1:
+        finalDG = pd.DataFrame.from_records([finaldict])
+    else:
+        tmp = pd.DataFrame.from_records([finaldict])
+        finalDG = finalDG.append(tmp, ignore_index=True)
+
+import  pickle
+with  open("myData.pickle","wb") as oFile:
+    pickle.dump(finalDG, oFile)
+    
+    
